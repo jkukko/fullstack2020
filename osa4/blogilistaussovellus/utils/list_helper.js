02d6489adcd8292a12aa6211395feb61372e94a6
jkukko <joonas.kukkonen@gmail.com>
2020-10-26 13:33:37 +0200

    exercise 4.5

--- osa4/blogilistaussovellus/utils/list_helper.js
+++ osa4/blogilistaussovellus/utils/list_helper.js
@@ -6,8 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((s, b) => s + b.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const result = blogs.reduce((max, b) => max && max.likes > b.likes ? max : b, null)
+  return {
+    title: result.title,
+    author: result.author,
+    likes: result.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
 
