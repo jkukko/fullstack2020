8f840349c0c2fe89e6a84ccd8f91cc810179da64
Joonas Kukkonen <kukkojoo@Joonass-MacBook-Pro.local>
2021-02-27 14:09:36 +0200

    fixed user

--- osa4/blogilistaussovellus/controllers/blogs.js
+++ osa4/blogilistaussovellus/controllers/blogs.js
@@ -32,6 +32,8 @@ blogsRouter.post('/', async (request, response) => {
 
   const blog = new Blog(body)
 
+  blog.user = user._id
+
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
