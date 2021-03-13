9548ae22b267d11e235250010cb4abfcd0618d25
Joonas Kukkonen <kukkojoo@Joonass-MacBook-Pro.local>
2021-03-13 11:59:24 +0200

    Exercise 6.2

--- osa6/unicafe-redux/src/index.js
+++ osa6/unicafe-redux/src/index.js
@@ -5,22 +5,62 @@ import reducer from './reducer'
 
 const store = createStore(reducer)
 
+const Statistics = ({ good, ok, bad }) => {
+
+  const total_feedback = good + ok + bad
+  const ave = `${Math.round((good + (-1) * bad) / total_feedback * 10)}` / 10
+  const positive = `${Math.round((good / total_feedback)*100)}%`
+
+  if(total_feedback === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <div>good {good}</div>
+      <div>ok {ok}</div>
+      <div>bad {bad}</div>
+      <div>feedback given {total_feedback}</div>
+      <div>average {ave}</div>
+      <div>positive {positive}</div>
+    </div>
+  )
+}
+
 const App = () => {
   const good = () => {
     store.dispatch({
       type: 'GOOD'
     })
   }
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
 
   return (
     <div>
+      <h2>Give Feedback</h2>
       <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <button onClick={ok}>neutral</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <Statistics good={store.getState().good} ok={store.getState().ok} bad={store.getState().bad}/>
     </div>
   )
 }
